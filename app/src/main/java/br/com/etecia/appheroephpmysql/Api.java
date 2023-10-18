9cc0d6ee1ceb27f260405c8d3aee2d256841ecaf
dvctrl <davidmouzinho.contato@gmail.com>
2023-10-17 21:04:35 -0300

    first commit

--- app/src/main/java/br/com/etecia/appheroephpmysql/Api.java
+++ app/src/main/java/br/com/etecia/appheroephpmysql/Api.java
@@ -1,7 +1,7 @@
 package br.com.etecia.appheroephpmysql;
 
 public class Api {
-    private static final String ROOT_URL = "http://10.67.96.236/HeroApi/v1/Api.php?apicall=";
+    private static final String ROOT_URL = "http://10.67.96.175/HeroeApi/v1/Api.php?apicall=";
 
     public static final String URL_CREATE_HERO = ROOT_URL + "createhero";
     public static final String URL_READ_HEROES = ROOT_URL + "getheroes";
